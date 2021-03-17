317119f6cffe35a205529d72db18c5c9e1c4d073
Mads Mogensen <mail@madsmogensen.dk>
2021-03-17 10:46:13 +0100

    fix lint

--- src/math.js
+++ src/math.js
@@ -1,13 +1,12 @@
 module.exports = plus;
 /**
  * @summary Adds two numbers together
- * @param {number} a
- * @param {number} b
+ * @param {number} a The number to add to b
+ * @param {number} b The number to add to a
  * @returns {number} The result
  * @throws {TypeError} On invalid arguments
  */
-function plus(a, b)
-{
+function plus(a, b) {
     if (typeof a !== "number") { throw new TypeError("a is not a number"); }
     if (typeof b !== "number") { throw new TypeError("b is not a number"); }
 
