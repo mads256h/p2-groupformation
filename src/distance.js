b19e52960064a2f8d8ef50a6c019cd1caa6b594f
thom776g <thomas@nordfiber.dk>
2021-03-29 11:41:36 +0200

    Endnu mere testing af distance algoritme
    title

--- src/distance.js
+++ src/distance.js
@@ -6,15 +6,14 @@ module.exports = {maxDistance};
  */
 function maxDistance(criteria){
     let value = 0;
-    let max = -Infinity, min = Infinity;
 
     for(let i = 0; i < criteria[0].length; i++){
-        
+        let max = -Infinity, min = Infinity;
         for(let student = 0; student < criteria.length; student++){
             min = Math.min(min, criteria[student][i]);
             max = Math.max(max, criteria[student][i]);
         }
-        value += max + (-min)
+        value += max + (-min);
     }
     return value/criteria[0].length;
 }
--- src/distance.test.js
+++ src/distance.test.js
@@ -1,5 +1,6 @@
 const {maxDistance} = require("./distance.js");
 test("Test return values of max distance", () => {
+    let cToSeks = [[9, 7, 9, 3],[5, -3, 1, -5],[1, 5, 5, -3],[-5, -7, 3, -9],[7, -7, 11, -9],[1, 5, 5, -7],[9, 5, 5, -3]];
     let testgroup1 = [[-11],[11]];
     let testgroup2 = [[-11],[-11]];
     let testgroup3 = [[11],[-11]];
@@ -8,11 +9,11 @@ test("Test return values of max distance", () => {
 
     let testgroup6 = [[-11, 11],[-11,5],[5,2],[7,-7]];
     let testgroup7 = [[-11, 10, 7, 5],[-11, 2, -9, 3],[5, 6, 4, 2],[7, -7, -8, 5]];
-    // let testgroup8 = [[-11],[-11],[5],[7]];
-    // let testgroup9 = [[-11],[-11],[5],[7]];
+    //let testgroup8 = [[-11, 9, 7],[-11, 2, 4],[5, 11, 4],[7, 2]];
     // let testgroup10 = [[-11],[-11],[5],[7]];
     // let testgroup11 = [[-11],[-11],[5],[7]];
 
+    expect(maxDistance(cToSeks)).toEqual(12.5);
     expect(maxDistance(testgroup1)).toEqual(22);
     expect(maxDistance(testgroup2)).toEqual(0);
     expect(maxDistance(testgroup3)).toEqual(22);
@@ -20,10 +21,8 @@ test("Test return values of max distance", () => {
     expect(maxDistance(testgroup5)).toEqual(0);
 
     expect(maxDistance(testgroup6)).toEqual(18);
-    expect(maxDistance(testgroup7)).toEqual(13,5);
-    // expect(maxDistance(testgroup8)).toEqual(22);
-    // expect(maxDistance(testgroup9)).toEqual(18);
+    expect(maxDistance(testgroup7)).toEqual(13.5);
+    //expect(maxDistance(testgroup8)).toEqual(22);
     // expect(maxDistance(testgroup10)).toEqual(0);
     // expect(maxDistance(testgroup11)).toEqual(22);
 });
-
