714c2a14392f719018dbcefdbf9544ef7b42c642
henneboy <henrik_peet@hotmail.com>
2021-03-29 09:14:58 +0200

    Removed this. at request of the lord and programmer Mads
     Ekstre-Mads: Eeeeh det er fordi at eslint er sat op til node og ikke browseren
    Ekstre-Mads: Det virker alligevel
    Ekstre-Mads: document eksisterer ikke i node
    Henrik van Piiiiiiiit: Ok, skal jeg bare ignore dem?
    Ekstre-Mads: Ja så fixer jeg det i mellemtiden

--- src/visualiser/svg.js
+++ src/visualiser/svg.js
@@ -1,6 +1,6 @@
-this.document.addEventListener("DOMContentLoaded", () =>{ //mads please help :) skal der stå "this."
+document.addEventListener("DOMContentLoaded", () =>{ //mads please help :) skal der stå "this."
 //Dette vil være main
-    let para = this.document.createElement("P");
+    let para = document.createElement("P");
     para.innerText = "This is a paragraph.";
     this.document.body.appendChild(para);
     //let svg = SVGAElement() undersøg dette
