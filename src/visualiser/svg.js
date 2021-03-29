a7626960630ceeeff2f4744ec5c48264c96dc01c
henneboy <henrik_peet@hotmail.com>
2021-03-29 15:16:58 +0200

    Mere svg
    nu med lines
    +farvede cirkler
    +en grå baggrund, så man kan se hvor stor svg-elementet er

--- src/visualiser/svg.js
+++ src/visualiser/svg.js
@@ -1,13 +1,20 @@
+const RANGEWIDTH = 22;
+let svgWidth = window.innerWidth/2;
+let svgLineSpace = svgWidth*0.05; // the amount of space between each line
+let svgHeight = 5*svgLineSpace;
+
 document.addEventListener("DOMContentLoaded", () =>{
+    console.log("TEST");
     let para = document.createElement("P");
     para.innerText = "Svg area is grey";
     document.body.appendChild(para);
 
+    let colorArr = ["blue", "green", "red", "yellow", "lime", "orange", "magenta", "brown", "pink", "cyan", "purple", "hotpink", "chartreuse"];
+    let learningStyles = 4; //array med læringsstile
+    let arr2d =[[-11,5,6,7], [-11,6,7,11], [2,2,2,2], [3,3,3,3], [4,4,4,4]]; //array med læringsstile
     //Dette vil være main
-    const RANGEWIDTH = 22;
-    let svgWidth = window.innerWidth/2;
-    let svgLineSpace = 20; // the amount of space between each line
-    let svgHeight = 5*svgLineSpace;
+    //let svgOfset = 25;
+    let svgOfset = svgLineSpace;
     let svgElement = document.createElement("DIV");
     document.body.appendChild(svgElement);
 
@@ -16,25 +23,14 @@ document.addEventListener("DOMContentLoaded", () =>{
     svg1.setAttribute("width", svgWidth);
     svg1.setAttribute("height", svgHeight);
     svg1.appendChild(createRect(0, 0, svgWidth, svgHeight)); // create a lightgrey rect that fills the whole svg to show it in browser.
-
-
-    // create a shape
-    /*    const cir1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    cir1.setAttribute("cx", 50 );
-    cir1.setAttribute("cy", 50 );
-    cir1.setAttribute("r", 50);
-    */
-
-    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    line.setAttribute("x1", "0");
-    line.setAttribute("y1", "0");
-    line.setAttribute("x2", "50");
-    line.setAttribute("y2", "50");
-    line.setAttribute("style", "stroke: black; stroke-width: 10px;");
-
-    //    svg1.appendChild(rect);
-    svg1.appendChild(line);
-    //svg1.appendChild(cir1);
+    for (let i = 1; i <= learningStyles; i++) {
+        svg1.appendChild(createLine(svgOfset, svgLineSpace*i, svgWidth-svgOfset));
+        svg1.appendChild(createCircle(svgOfset, svgLineSpace*i));
+        for (let j = 0; j < arr2d.length; j++) {
+            svg1.appendChild(createCircle(cirXVal(arr2d[j][i-1]), svgLineSpace*i, colorArr[j]));
+        }
+    }
+    //svg1.appendChild(createLine(0, 10, svgWidth)); // create a lightgrey rect that fills the whole svg to show it in browser.
 
     // attach the master svg to the element on page
     svgElement.appendChild(svg1);
@@ -74,17 +70,72 @@ function createRect(x, y, width, height, color){
  * @param {number} width the widthe of the rectangle
  * @returns {Element} line svg element?
  */
-function createLine(x1, y1, x2, y2, width){
-    if (width === undefined){
-        width = 10;
-    }
+function createLine(x1, y1, x2, y2=y1, width=window.innerWidth*0.005){
     const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
     line.setAttribute("x1", x1);
     line.setAttribute("y1", y1);
     line.setAttribute("x2", x2);
     line.setAttribute("y2", y2);
     line.setAttribute("stroke-width", width);
-    //line.setAttribute("style", "stroke: black; stroke-width: 10px;");
+    line.setAttribute("style", "stroke: black;");
     return line;
 }
 
+/**
+ * @summary Creates and return a svg cicle
+ * @param {number} cx the x position of the center of the circle
+ * @param {number} cy the y position of the center of the circle
+ * @param {color} color the color of the circle
+ * @param {number} r the radius of the circle
+ * @returns {Element} circle svg element?
+ */
+function createCircle(cx, cy, color = "cyan", r=window.innerWidth*0.01,){
+    const cir = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    cir.setAttribute("cx", cx);
+    cir.setAttribute("cy", cy);
+    cir.setAttribute("r", r);
+    cir.setAttribute("fill", color);
+    return cir;
+}
+
+/**
+ * @summary Creates and return a svg circle which is hollow
+ * @param {number} cx the x position of the center of the circle
+ * @param {number} cy the y position of the center of the circle
+ * @param {color} color the color of the circle
+ * @returns {Element} circle svg element?
+ */
+function createHollowCircle(cx, cy, r, color = "cyan", width=window.innerWidth*0.01){
+    const cir = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    cir.setAttribute("cx", cx);
+    cir.setAttribute("cy", cy);
+    cir.setAttribute("r", r);
+    //cir.setAttribute("fill", color);
+    cir.setAttribute("stroke-width", width);
+    cir.setAttribute("stroke", color);
+    cir.setAttribute("fill", "none");
+    /*cir.setAttribute("fill", "none");
+    cir.setAttribute("stroke", "red"); */
+    return cir;
+}
+
+function cirXVal(value){
+    let x = (window.innerWidth/2)*0.05; //add the ofset from the left of the svg (the lines start some length inside the grey)
+    x+=((window.innerWidth/2)-(2*(window.innerWidth/2)*0.05))*((value+11)/RANGEWIDTH);
+    console.log(x);
+    return x;
+}
+
+let arr = [1, 1, 4, 5, 6, 7, 8];
+hyppig(arr);
+function hyppig(arr){
+    let arrHyp;
+    for (let i = 0; i < arr.length; i++){
+        if (arrHyp[arr[i]] === undefined){
+            arrHyp[arr[i]] = 1;
+        }
+        else {
+            arrHyp[arr[i]]++;
+        }
+    }
+}
