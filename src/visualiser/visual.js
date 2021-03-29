c3395f76d775d075d496f030b15614e2c4a8e7ce
S3JER <72788875+S3JER@users.noreply.github.com>
2021-03-29 11:35:13 +0200

    Update visual.js

--- src/visualiser/visual.js
+++ src/visualiser/visual.js
@@ -5,8 +5,8 @@
  * @returns {number} The result of the two numbers added togther
  * @throws {TypeError} On invalid arguments
  */
-function absoluteValueOfTwoNumbers(NumberOne, NumberTwo){
-    return Math.abs(NumberOne)+Math.abs(NumberTwo);
+function absoluteValueOfTwoNumbers(NumberOne, NumberTwo) {
+    return Math.abs(NumberOne) + Math.abs(NumberTwo);
 }
 
 /**
@@ -15,12 +15,12 @@ function absoluteValueOfTwoNumbers(NumberOne, NumberTwo){
  * @returns {number} Distance between the max and min value in the array
  * @throws {TypeError} On invalid arguments
  */
-function maxsMin(array){
+function maxsMin(array) {
     let newArray = array.slice();
-    newArray.sort(function(a, b){
+    newArray.sort(function (a, b) {
         return a - b;
     });
-    return absoluteValueOfTwoNumbers(newArray[0], newArray[newArray.length-1]);
+    return absoluteValueOfTwoNumbers(newArray[0], newArray[newArray.length - 1]);
 }
 
 /**
@@ -29,11 +29,74 @@ function maxsMin(array){
  * @returns {number} The result of the sum
  * @throws {TypeError} On invalid arguments
  */
-function sum(array){
-    return array.reduce((a, b) => a+b, 0);
+function sumOfArray(array){
+    return array.reduce((a, b) => a + b, 0);
 }
-let eksample = [1, -11 , 11, 2];
+
+/**
+ * @summary Finds the length between the numbers
+ * @param {Array}array Array of numbers
+ * @returns {number} The sum of all the lengths between the numbers
+ * @throws {TypeError} On invalid arguments
+ */
+function distribution(array) {
+    if (array.length === 1) {
+        console.log("for få argrumenter");
+    }
+    else {
+        let newArray = array.slice();
+        newArray.sort(function (a, b) {
+            return a - b;
+        });
+        let sum = 0;
+        let pef = perfectDist(newArray.length);
+        for (let index = 0; index < newArray.length; index++) {
+            sum += newArray[index] - pef[index];
+        }
+        return sum;
+    }
+}
+
+/**
+ * @summary Makes the perfect array of numbers with a perfect distances between
+ * @param {Array} n The number of indexs needed in the array
+ * @returns {Array} The calulated array
+ * @throws {TypeError} On invalid arguments
+ */
+function perfectDist(n) {
+    let array = new Array();
+    array[0] = -11;
+    const rangeWidth = 22;
+    for (let i = 1; i <= n - 1; i++) {
+        array[i] = ((rangeWidth / (n - 1)) * i) - 11;
+    }
+    return array;
+}
+
+console.log(perfectDist(7));
+let eksampleBad = [
+    -1,
+    -7.333333333333334,
+    -3.666666666666667,
+    0,
+    3.666666666666666,
+    7.333333333333332,
+    11
+];
+let eksamplePef = [
+    -11,
+    -7.333333333333334,
+    -3.666666666666667,
+    0,
+    3.666666666666666,
+    7.333333333333332,
+    11
+];
+
+distribution(eksampleBad);
+distribution(eksamplePef);
+
 //let perfekt = [11, 3, -3 -11];
 
-console.log(maxsMin(eksample));
-console.log(sum(eksample));
+//console.log(maxsMin(eksample));
+//console.log(sum(eksample));
