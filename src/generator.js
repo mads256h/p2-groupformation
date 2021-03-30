2ff6877d4dfa95bb6f2cdff4e804dcd7067e6a19
Casper-NS <37834568+Casper-NS@users.noreply.github.com>
2021-03-30 14:37:55 +0200

    Update generator.js

--- src/generator.js
+++ src/generator.js
@@ -1,17 +1,17 @@
 const {Group, Student, Criteria, LearningStyles, SubjectPreference, Subject} = require("./group");
 
-let argv = process.argv;
+let argv = process.argv.splice(2);
 
 /**
  * @summary A function used to generate a randomized student, these will be destributed into groups
- * @param {number} id a number to identify the student, used instead of a name for now.
- * @returns {Student} Returns a student with randomized criteria
  */
-function studentGenerator(id){
+function studentGenerator(){
     let learningStyle = new LearningStyles(lsGenerator(), lsGenerator(), lsGenerator(), lsGenerator());
     let criteria = new Criteria(Math.random(), Math.floor(Math.random()*3), learningStyle, null);
-    let student = new Student(id.toString, criteria);
-    return student;
+    let studentArray = [];
+    for (let index = 0; index < argv[0]; index++) {
+        studentArray.push(new Student(index.toString, criteria));
+    }
 }
 
 /**
@@ -22,11 +22,3 @@ function lsGenerator(){
     let ls = [-11, -9, -7, -5, -3, -1, 1, 3, 5, 7, 9, 11];
     return ls[Math.floor(Math.random()*13)];
 }
-
-function groupGenerator(){
-    for (let index = 0; index < argv[0]; index++) {
-        console.log("hello, argv test");
-    }
-}
-
-groupGenerator();
