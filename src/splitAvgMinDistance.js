fdcddf77a4dc48f7f68acdb869408f1cd64d6794
thom776g <thomas@nordfiber.dk>
2021-03-30 12:28:19 +0200

    Update splitAvgMinDistance.js

--- src/splitAvgMinDistance.js
+++ src/splitAvgMinDistance.js
@@ -1,12 +1,24 @@
 module.exports = {splitAvgMinDistance};
-const {inputCheck} = require("./inputCheck.js");
+
+const {
+    assertArray,
+    assertArrayItemsInstanceOf,
+    assertArrayLengthEq,
+    assertArraysOfArrayNotEmpty,
+    assertArrayNotEmpty
+} = require("./typeassert");
 /**
  * @summary Measures the max distance between all criterias.
  * @param {number[][]} criteria 2d array with students and their criterias
  * @returns {number} the calculated score
  */
 function splitAvgMinDistance(criteria){
-    inputCheck(criteria);
+    assertArray(criteria);
+    assertArrayNotEmpty(criteria);
+    assertArrayItemsInstanceOf(criteria, Array);
+    assertArraysOfArrayNotEmpty(criteria);
+    assertArrayLengthEq(...criteria);
+
     let sumDist = 0;
 
     for(let i = 0; i < criteria[0].length; i++){
