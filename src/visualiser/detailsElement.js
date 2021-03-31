c480eab7146bc171d825573dc56344142773c4af
S3JER <72788875+S3JER@users.noreply.github.com>
2021-03-31 10:25:20 +0200

    DOM manipulation
    4 elements, details, maxMine, linearDist and sum

--- src/visualiser/detailsElement.js
+++ src/visualiser/detailsElement.js
@@ -1,62 +1,88 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    let para = document.createElement("details");
-    let summary = document.createElement("summary");
-    summary.innerText = "Students";
-    para.appendChild(summary);
-    let p = document.createElement("P");
-    p.appendChild(printStudents(groupTest));
-    para.appendChild(p);
-    document.body.appendChild(para);
+(function () {
+    const {
+        distribution,
+        sumOfArray,
+        distanceBetweenExtremes
+    } = window.visualjs;
 
-    //The div element that are placed to the right in the svg div
-    let divElement = document.createElement("div");
-    const rightToSvg  = document.querySelector("div > svg");
-    divElement.className = "groupsize";
-    rightToSvg.after(divElement);
 
-    //The maxMine functions output inserted in the groupsize class
-    let maxMine = document.createElement("P");
-    maxMine.innerText = "MaxMine:";
-    divElement.appendChild(maxMine);
+    document.addEventListener("DOMContentLoaded", () => {
+        createVisualInfo(groupTest, exsampleBad);
+    });
 
-    //The sum functions output inserted in the groupsize class under maxMine
-    let sum = document.createElement("P");
-    sum.innerText = "The sum:";
-    divElement.appendChild(sum);
+    /**
+     * @summary Creates the html code for the information in the groups
+     * @param {object} group The class with all the groups
+     * @param {number[]} array Array of the students learningstyles
+     */
+    function createVisualInfo(group, array) {
+        //The div element that are placed to the right in the svg div
+        let divElement = document.createElement("div");
+        const rightToSvg = document.querySelector("div > svg");
+        divElement.className = "groupsize";
+        rightToSvg.after(divElement);
 
-    //The linearDist function output inserted in the groupsize class under sum
-    let linearDist = document.createElement("P");
-    linearDist.innerText = "Linear distribution:";
-    divElement.appendChild(linearDist);
-});
+        //The maxMine functions output inserted in the groupsize class
+        let maxMine = document.createElement("P");
+        maxMine.innerText = "MaxMine: " + distanceBetweenExtremes(array).toString();
+        divElement.appendChild(maxMine);
 
-let groupTest = {
-    name: "Group 1",
-    id: 1,
-    students: [
-        {
-            name: "Morten"
-        },
-        {
-            name: "Mads"
-        },
-        {
-            name: "Sven"
-        }
-    ]
-};
+        //The sum functions output inserted in the groupsize class under maxMine
+        let sum = document.createElement("P");
+        sum.innerText = "The sum: " + sumOfArray(array).toString();
+        divElement.appendChild(sum);
 
-function printStudents(group){
-    const list = document.createElement("ul");
-    for(let student of group.students){
-        const listItem = document.createElement("li");
-        listItem.innerText = student.name;
-        list.appendChild(listItem);
+        //The linearDist function output inserted in the groupsize class under sum
+        let linearDist = document.createElement("P");
+        linearDist.innerText = "Linear distribution: " + distribution(array).toString();
+        divElement.appendChild(linearDist);
+
+        //Details element
+        let details = document.createElement("details");
+        let summary = document.createElement("summary");
+        summary.innerText = "Students";
+        details.appendChild(summary);
+        details.appendChild(printStudents(group));
+        divElement.after(details);
     }
-    return list;
-};
 
-
-
-
+    /**
+     * @summary Prints the names of the students in a group
+     * @param {object} group Takes a class of groups with all the students in the class
+     * @returns {string} A string with the names in a unorded list
+     */
+    function printStudents(group) {
+        const list = document.createElement("ul");
+        for (let student of group.students) {
+            const listItem = document.createElement("li");
+            listItem.innerText = student.name;
+            list.appendChild(listItem);
+        }
+        return list;
+    }
+    let groupTest = {
+        name: "Group 1",
+        id: 1,
+        students: [
+            {
+                name: "Morten"
+            },
+            {
+                name: "Mads"
+            },
+            {
+                name: "Sven"
+            }
+        ]
+    };
+    let exsampleBad = [
+        -1,
+        -7.333333333333334,
+        -3.666666666666667,
+        0,
+        3.666666666666666,
+        7.333333333333332,
+        11
+    ];
+}());
 
--- src/visualiser/visual.js
+++ src/visualiser/visual.js
@@ -1,87 +1,95 @@
-/**
- * @summary Finds the distance between the min and max value in the array
- * @param {number[]} array Array of numbers
- * @returns {number} Distance between the max and min value in the array
- */
-function distanceBetweenExtremes(array) {
-    const min = Math.min(...array);
-    const max = Math.max(...array);
-    return Math.abs(min) + Math.abs(max);
-}
-
-/**
- * @summary Finds the sum
- * @param {number[]} array Array of numbers
- * @returns {number} The result of the sum
- */
-function sumOfArray(array){
-    return array.reduce((a, b) => a + b, 0);
-}
-
-/**
- * @summary Finds the length between the numbers
- * @param {number[]} array Array of numbers
- * @returns {number} The sum of all the lengths between the numbers
- * @throws {RangeError} On invalid arguments
- */
-function distribution(array) {
-    if (array.length === 1) {
-        throw new RangeError("To few arguments");
+(function (){
+    /**
+     * @summary Finds the distance between the min and max value in the array
+     * @param {number[]} array Array of numbers
+     * @returns {number} Distance between the max and min value in the array
+     */
+    function distanceBetweenExtremes(array) {
+        const min = Math.min(...array);
+        const max = Math.max(...array);
+        return Math.abs(min) + Math.abs(max);
     }
-    else {
-        let newArray = array.slice();
-        newArray.sort(function (a, b) {
-            return a - b;
-        });
-        let sum = 0;
-        let pef = perfectDist(newArray.length);
-        for (let index = 0; index < newArray.length; index++) {
-            sum += Math.abs(newArray[index] - pef[index]);
+
+    /**
+     * @summary Finds the sum
+     * @param {number[]} array Array of numbers
+     * @returns {number} The result of the sum
+     */
+    function sumOfArray(array){
+        return array.reduce((a, b) => a + b, 0);
+    }
+
+    /**
+     * @summary Finds the length between the numbers
+     * @param {number[]} array Array of numbers
+     * @returns {number} The sum of all the lengths between the numbers
+     * @throws {RangeError} On invalid arguments
+     */
+    function distribution(array) {
+        if (array.length === 1) {
+            throw new RangeError("To few arguments");
         }
-        return sum;
+        else {
+            let newArray = array.slice();
+            newArray.sort(function (a, b) {
+                return a - b;
+            });
+            let sum = 0;
+            let pef = perfectDist(newArray.length);
+            for (let index = 0; index < newArray.length; index++) {
+                sum += Math.abs(newArray[index] - pef[index]);
+            }
+            return sum;
+        }
     }
-}
 
-/**
- * @summary Makes the perfect array of numbers with a perfect distances between
- * @param {number} n The number of indexs needed in the array
- * @returns {Array} The calulated array
- */
-function perfectDist(n) {
-    let array = new Array();
-    array[0] = -11;
-    const rangeWidth = 22;
-    for (let i = 1; i <= n - 1; i++) {
-        array[i] = ((rangeWidth / (n - 1)) * i) - 11;
+    /**
+     * @summary Makes the perfect array of numbers with a perfect distances between
+     * @param {number} n The number of indexs needed in the array
+     * @returns {Array} The calulated array
+     */
+    function perfectDist(n) {
+        let array = new Array();
+        array[0] = -11;
+        const rangeWidth = 22;
+        for (let i = 1; i <= n - 1; i++) {
+            array[i] = ((rangeWidth / (n - 1)) * i) - 11;
+        }
+        return array;
     }
-    return array;
-}
 
-console.log(perfectDist(7));
-let eksampleBad = [
-    -1,
-    -7.333333333333334,
-    -3.666666666666667,
-    0,
-    3.666666666666666,
-    7.333333333333332,
-    11
-];
-let eksamplePef = [
-    -11,
-    -7.333333333333334,
-    -3.666666666666667,
-    0,
-    3.666666666666666,
-    7.333333333333332,
-    11
-];
+    console.log(perfectDist(7));
+    let eksampleBad = [
+        -1,
+        -7.333333333333334,
+        -3.666666666666667,
+        0,
+        3.666666666666666,
+        7.333333333333332,
+        11
+    ];
+    let eksamplePef = [
+        -11,
+        -7.333333333333334,
+        -3.666666666666667,
+        0,
+        3.666666666666666,
+        7.333333333333332,
+        11
+    ];
 
-distribution(eksampleBad);
-distribution(eksamplePef);
-console.log(distribution(eksampleBad));
-console.log(distribution(eksamplePef));
-//let perfekt = [11, 3, -3 -11];
+    distribution(eksampleBad);
+    distribution(eksamplePef);
+    console.log(distribution(eksampleBad));
+    console.log(distribution(eksamplePef));
+    //let perfekt = [11, 3, -3 -11];
 
-//console.log(maxsMin(eksample));
-//console.log(sum(eksample));
+    //console.log(maxsMin(eksample));
+    //console.log(sum(eksample));
+
+    window.visualjs = {
+        distribution,
+        sumOfArray,
+        distanceBetweenExtremes
+    };
+}());
