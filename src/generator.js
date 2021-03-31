270616d915f993b7bec65ba6c1be9316e764b88c
Mads Mogensen <mail@madsmogensen.dk>
2021-03-31 13:47:51 +0200

    generator: fixes

--- src/generator.js
+++ src/generator.js
@@ -1,7 +1,10 @@
+#!/usr/bin/node
+
 const {Student, Criteria, LearningStyles, Subject, SubjectPreference} = require("./group");
+const {assertArray, assertArrayItemsInstanceOf, assertString, assertStringNotEmpty} = require("./typeassert");
 const fs = require("fs");
 
-let argv = process.argv.splice(2);
+const argv = process.argv.splice(2);
 
 if (argv.length !== 3){
     throw new RangeError("Wrong number of arguments");
@@ -14,7 +17,7 @@ if (argv.length !== 3){
  * @returns {Student[]} returns an array of students.
  */
 function studentGenerator(numOfStudents, numOfSubjects){
-    let studentArray = [];
+    const studentArray = [];
     for (let index = 0; index < numOfStudents; index++) {
         const learningStyle = new LearningStyles(lsGenerator(), lsGenerator(), lsGenerator(), lsGenerator());
         const criteria = new Criteria(Math.floor(Math.random() * 10) + 1, Math.floor(Math.random() * 3), learningStyle, subjectGenerator(numOfSubjects));
@@ -28,7 +31,7 @@ function studentGenerator(numOfStudents, numOfSubjects){
  * @returns {number} The return is a random learning style number
  */
 function lsGenerator(){
-    let ls = [-11, -9, -7, -5, -3, -1, 1, 3, 5, 7, 9, 11];
+    const ls = [-11, -9, -7, -5, -3, -1, 1, 3, 5, 7, 9, 11];
     return ls[Math.floor(Math.random() * 12)];
 }
 
@@ -38,7 +41,7 @@ function lsGenerator(){
  * @returns {SubjectPreference} Returns an array of subjects.
  */
 function subjectGenerator(numOfSubjects){
-    let subjects = [];
+    const subjects = [];
     for (let index = 0; index < numOfSubjects; index++) {
         subjects.push(new Subject((index + 1).toString(), Math.random()));
     }
@@ -47,12 +50,17 @@ function subjectGenerator(numOfSubjects){
 
 /**
  * @summary Saves all of the students into a json file.
- * @param {Array} students Takes an array of students as input.
+ * @param {Student[]} students Takes an array of students as input.
  * @param {string} fileName Input to dictate name of file that is saved.
  */
 function saveToFile(students, fileName){
-    let data = JSON.stringify(students, null, 2);
-    fs.writeFile(fileName + ".JSON", data, (err) => {
+    assertArray(students);
+    assertArrayItemsInstanceOf(students, Student);
+    assertString(fileName);
+    assertStringNotEmpty(fileName);
+
+    const data = JSON.stringify(students, null, 2);
+    fs.writeFile(fileName, data, (err) => {
         if (err) {
             throw err;
         }
