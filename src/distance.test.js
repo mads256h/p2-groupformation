fdc52862e4b3606b3e9894b150b743eb04ff47a7
thom776g <thomas@nordfiber.dk>
2021-03-31 10:34:08 +0200

    Review fix update

--- src/distance.test.js
+++ src/distance.test.js
@@ -1,6 +1,13 @@
 const {maxDistance} = require("./distance.js");
 test("return values of max distance", () => {
-    let cToSeks = [[9, 7, 9, 3],[5, -3, 1, -5],[1, 5, 5, -3],[-5, -7, 3, -9],[7, -7, 11, -9],[1, 5, 5, -7],[9, 5, 5, -3]];
+    let cToSeks = [
+        [9, 7, 9, 3],
+        [5, -3, 1, -5],
+        [1, 5, 5, -3],
+        [-5, -7, 3, -9],
+        [7, -7, 11, -9],
+        [1, 5, 5, -7],
+        [9, 5, 5, -3]];
     let testgroup1 = [[-11], [11]];
     let testgroup2 = [[-11], [-11]];
     let testgroup3 = [[11], [-11]];
@@ -8,7 +15,11 @@ test("return values of max distance", () => {
     let testgroup5 = [[-11]];
 
     let testgroup6 = [[-11, 11], [-11, 5], [5, 2], [7, -7]];
-    let testgroup7 = [[-11, 10, 7, 5], [-11, 2, -9, 3], [5, 6, 4, 2], [7, -7, -8, 5]];
+    let testgroup7 = [
+        [-11, 10, 7, 5],
+        [-11, 2, -9, 3],
+        [5, 6, 4, 2],
+        [7, -7, -8, 5]];
 
 
     expect(maxDistance(cToSeks)).toEqual(12.5);
