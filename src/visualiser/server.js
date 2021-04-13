11bf58ef12c0828db3f4bb32bff1ac0e249d492d
Mads Mogensen <mail@madsmogensen.dk>
2021-04-13 14:16:47 +0200

    webserver: spicy things

--- src/visualiser/server.js
+++ src/visualiser/server.js
@@ -8,10 +8,11 @@ const fsp = require("fs").promises;
 const webserver = new WebServer("localhost", 3000, __dirname);
 
 webserver.addGetHandler("/ls", lsHandler);
+webserver.addPostHandler("/ls", lsHandler);
 webserver.run().then(() => console.log("Server started on http://localhost:3000"));
 
 
-function lsHandler(getParams, request) {
+function lsHandler(data, request) {
     return fsp.readdir(path.join(__dirname, "/data"))
         .catch((e) => {throw new HttpError(500, e.message)});
 }
