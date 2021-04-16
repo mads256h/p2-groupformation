c634c216ccf7d7e77095dd74f3f1c8ad6545722e
thom776g <thomas@nordfiber.dk>
2021-04-16 10:47:05 +0200

    preferenceAlg

--- src/algorithms/masterAlgorithm.js
+++ src/algorithms/masterAlgorithm.js
@@ -5,3 +5,18 @@
  * @author Mati-AAU and thom776g
  */
 
+function prefrenceAlg(preferences){
+    let fromHome = 0, inOffice = 0;
+    for (const preference of preferences) {
+        if(preference < -Number.EPSILON){
+            fromHome++;
+        }
+        if(preference > Number.EPSILON){
+            inOffice++;
+        }
+    }
+    let min = Math.min(fromHome, inOffice)
+    let max = Math.max(fromHome, inOffice)
+    
+    return -(min/max);
+}
