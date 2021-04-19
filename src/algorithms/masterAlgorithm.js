8ba472e02a2ca780bfad887d037132536ace65f6
Mati-AAU <matjen20@student.aau.dk>
2021-04-19 09:38:03 +0200

    fixed sloppyness

--- src/algorithms/masterAlgorithm.js
+++ src/algorithms/masterAlgorithm.js
@@ -5,38 +5,41 @@
  * @author Mati-AAU and thom776g
  */
 
- module.exports = {preferenceAlg};
- const {
-  assertArrayItemsType,
-  assertArrayNotEmpty
+module.exports = {preferenceAlg, masterAlg};
+const {
+    assertArrayItemsType,
+    assertArrayNotEmpty
 } = require("../typeassert");
-const { splitAvgMinDistance } = require("./splitAvgMinDistance");
-
-const{averageVectorDistance} = require("./vectorspace.js");
-const{constant} = require("./weightFunction.js");
-const{splitAvgMinDistance} = require("./splitAvgMinDistance.js")
 
 /**
  * @summary Calculates score for preferences (i.e. working from home)
- * @param {Number[]} preferences preference array. 0 is do not care, positive or negative is a preference. 
- * @returns Calculated score for preferences
+ * @param {number[]} preferences preference array. 0 is do not care, positive or negative is a preference. 
+ * @returns {number} Calculated score for preferences
  */
 function preferenceAlg(preferences){
+    assertArrayNotEmpty(preferences);
+    assertArrayItemsType(preferences, "number");
     let fromHome = 0, inOffice = 0;
     for (const preference of preferences) {
-        if(preference < -Number.EPSILON){
+        if (preference < -Number.EPSILON){
             fromHome++;
         }
-        if(preference > Number.EPSILON){
+        if (preference > Number.EPSILON){
             inOffice++;
         }
     }
-    let min = Math.min(fromHome, inOffice)
-    let max = Math.max(fromHome, inOffice)
-    
-    return -(min/max);
+    let min = Math.min(fromHome, inOffice);
+    let max = Math.max(fromHome, inOffice);
+    return -(min / max);
 }
 
+/**
+ * @summary Calculates the overall score based on input subfunctions
+ * @param {Function} heterogenousFunction Parameterless function for calculating heterogenous score
+ * @param {Function} homogenousFunction Parameterless function for calculating homogenous score
+ * @param {Function} subjectFunction Parameterless function for calculating subject score
+ * @returns {number} Calculated score for preferences
+ */
 function masterAlg(heterogenousFunction, homogenousFunction, subjectFunction){
     return heterogenousFunction() + homogenousFunction() + subjectFunction();
 }
