f3a4c99e132890632fdfff9eb0c41c02a6f7d965
thom776g <thomas@nordfiber.dk>
2021-04-19 10:23:39 +0200

    Fixede de dårlige spaces

--- src/groupSite/boxMaker.js
+++ src/groupSite/boxMaker.js
@@ -1,59 +1,56 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-  const divBox = makeDivElement("Hej");
-  document.body.appendChild(divBox);
-  const innerBox = makeDivElement("InnerBox");
-  innerBox.innerText = "Hej";
-  divBox.appendChild(innerBox);
-  const buttonInvite  = createButtons("invite");
-  
-  divBox.appendChild(buttonInvite);
-  const id = "invite";
-  divBox.appendChild(makeList(testArray));
-  document.getElementById(id).addEventListener("click", changeButtonColor(id));
+    const divBox = makeDivElement("Hej");
+    document.body.appendChild(divBox);
+    const innerBox = makeDivElement("InnerBox");
+    innerBox.innerText = "Hej";
+    divBox.appendChild(innerBox);
+    const buttonInvite  = createButtons("invite");
+    divBox.appendChild(buttonInvite);
+    const id = "invite";
+    divBox.appendChild(makeList(testArray));
+    document.getElementById(id).addEventListener("click", changeButtonColor(id));
 });
 
-
 function makeDivElement(name){
-  const div = document.createElement("div");
-  div.className = name;
-  return div;
+    const div = document.createElement("div");
+    div.className = name;
+    return div;
 }
 
 function makeList(groupArray){
-  const fullList = document.createElement("ul");
-  for(let group = 0; group < groupArray.length; group++){
-    const listPart = document.createElement("details");
-    const summary = document.createElement("summary");
-    summary.innerText = groupArray[group];
-    console.log(groupArray[group]);
-    console.log(summary);
-    listPart.appendChild(summary);
-    fullList.appendChild(listPart);
-  }
-  return fullList;
+    const fullList = document.createElement("ul");
+    for(let group = 0; group < groupArray.length; group++){
+        const listPart = document.createElement("details");
+        const summary = document.createElement("summary");
+        summary.innerText = groupArray[group];
+        console.log(groupArray[group]);
+        console.log(summary);
+        listPart.appendChild(summary);
+        fullList.appendChild(listPart);
+    }
+    return fullList;
 }
 
-
 function createListItem(innerText){
-  const listItem = document.createElement("li");
-  listItem.innerText = innerText;
-  return listItem;
+    const listItem = document.createElement("li");
+    listItem.innerText = innerText;
+    return listItem;
 }
 
 function createButtons(id) {
-  const button = document.createElement("button");
-  button.setAttribute("id", id);
-  button.innerText = "Invite";
-  return button;
+    const button = document.createElement("button");
+    button.setAttribute("id", id);
+    button.innerText = "Invite";
+    return button;
 }
 
 function changeButtonColor(id){
-  if(document.getElementById(id).style.backgroundColor === "white"){
-    document.getElementById(id).style.backgroundColor = "blue";
-  }
-  else{
-    document.getElementById(id).style.backgroundColor = "white";
-  }
+    if(document.getElementById(id).style.backgroundColor === "white"){
+        document.getElementById(id).style.backgroundColor = "blue";
+     }
+    else{
+        document.getElementById(id).style.backgroundColor = "white";
+    }
 }
 
 const testArray = ["Hej", "med", "dig"];
