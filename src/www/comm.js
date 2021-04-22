0ab1c08920814ed0f44adb6330b8b522497b35e8
Mads Mogensen <mail@madsmogensen.dk>
2021-04-22 14:04:35 +0200

    multiple updatehandler

--- src/www/comm.js
+++ src/www/comm.js
@@ -1,15 +1,17 @@
-(function() {
+(function () {
     function postRequest(url, postData) {
         return fetch("api/" + url, {
             method: "POST",
             body: new URLSearchParams(postData),
             headers: {"Content-Type": "application/x-www-form-urlencoded"}
-        }).then((response) => response.json());
+        }).then((response) => response.json())
+            .then((response) => {if (response.status !== "OK") {throw new Error(response.message)} return response;});
     }
 
     function getRequest(url, data = {}) {
         return fetch(`api/${url}?${new URLSearchParams(data)}`)
-            .then((response) => response.json());
+            .then((response) => response.json())
+            .then((response) => {if (response.status !== "OK") {throw new Error(response.message)} return response;});
     }
 
     function login(username) {
@@ -37,19 +39,33 @@
     }
 
 
+    const callbacks = [];
     function registerUpdateHandler(callback) {
+        callbacks.push(callback);
+    }
+
+    function executeCallbacks() {
+        callbacks.forEach((c) => c());
+    }
+
+    window.addEventListener("load", () => {
+        // Start websocket
         const socket = new WebSocket("ws://" + window.location.host + window.location.pathname);
-        socket.onerror = (e) => { 
+        socket.onerror = (e) => {
             console.error("Could not create websocket!", e);
             console.log("Using a timer instead!");
-            setInterval(callback, 1000);
+            setInterval(executeCallbacks, 1000);
         }
         socket.onopen = (e) => console.log("Opened websocket", e);
         socket.onclose = (e) => console.log("Closed websocket", e);
 
-        socket.onmessage = () => callback();
-    }
+        socket.onmessage = executeCallbacks;
+
+        // Start with calling the callbacks
+        executeCallbacks();
+    });
 
 
+    if (window.commjs) {console.error("commjs loaded twice"); alert("commjs loaded twice"); return;}
     window.commjs = {login, me, mygroup, rankedgroups, leavegroup, invitegroup, registerUpdateHandler};
 }());
