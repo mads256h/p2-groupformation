968838f5d7bcef97c0b297e5ded2960e0c535ff0
Mads Mogensen <mail@madsmogensen.dk>
2021-04-23 15:03:26 +0200

    commjs: add documentation

--- src/www/comm.js
+++ src/www/comm.js
@@ -1,53 +1,165 @@
-(function () {
+
+/**
+ * @description Formation website server communication module.
+ * @module comm
+ * @see module:comm
+ * @author mads256h
+ */
+
+(function() {
+    // Requests
+
+    /**
+     * @public
+     * @memberof module:comm
+     * @summary Send a login request
+     * @description This request sets a session cookie in the browser.
+     * This cookie is required in the all the other requests.
+     * @param {string} username The name of the student to log in as
+     * @returns {Promise<undefined>} The response
+     * @throws {Error} The status of the response is not "OK"
+     */
+    function login(username) {
+        return postRequest("login", {username});
+    }
+
+    /**
+     * @public
+     * @memberof module:comm
+     * @summary Get student information about the logged in student
+     * @description This request requires that a valid session cookie has been set
+     * @returns {Promise<module:group~Student>} A promise with the student
+     * @throws {Error} The status of the response is not "OK"
+     */
+    function me() {
+        return getRequest("me");
+    }
+
+    /**
+     * @public
+     * @memberof module:comm
+     * @summary Get group information about the logged in student
+     * @description This request requires that a valid session cookie has been set
+     * @returns {Promise<module:group~Group>} A promise with the students group
+     * @throws {Error} The status of the response is not "OK"
+     */
+    function mygroup() {
+        return getRequest("mygroup");
+    }
+
+    /**
+     * @typedef {object} RankedGroup
+     * @property {module:group~Group} group The group
+     * @property {number} value The value of the group
+     * @throws {Error} The status of the response is not "OK"
+     */
+
+    /**
+     * @public
+     * @memberof module:comm
+     * @summary Get a list of groups with a score
+     * @description This request requires that a valid session cookie has been set
+     * @returns {Promise<RankedGroup>} A promise with the ranked groups
+     * @throws {Error} The status of the response is not "OK"
+     */
+    function rankedgroups() {
+        return getRequest("rankedgroups");
+    }
+
+    /**
+     * @public
+     * @memberof module:comm
+     * @summary Leave the current group
+     * @description This request requires that a valid session cookie has been set
+     * @returns {Promise<undefined>} A promise
+     * @throws {Error} The status of the response is not "OK"
+     */
+    function leavegroup() {
+        return getRequest("leavegroup");
+    }
+
+    /**
+     * @public
+     * @memberof module:comm
+     * @summary Invite a group
+     * @description This request requires that a valid session cookie has been set
+     * @param {module:comm~Group} group The group to invite
+     * @returns {Promise<undefined>} A promise
+     * @throws {Error} The status of the response is not "OK"
+     */
+    function invitegroup(group) {
+        return postRequest("invitegroup", {groupid: group.id});
+    }
+
+
+    /**
+     * @summary Send a post request to url with postData
+     * @param {string} url The url to send the request to
+     * @param {object} postData The post data to send
+     * @returns {Promise<any>} The response
+     * @throws {Error} The status of the response is not "OK"
+     */
     function postRequest(url, postData) {
         return fetch("api/" + url, {
             method: "POST",
             body: new URLSearchParams(postData),
             headers: {"Content-Type": "application/x-www-form-urlencoded"}
         }).then((response) => response.json())
-            .then((response) => {if (response.status !== "OK") {throw new Error(response.message)} return response.response;});
+            .then((response) => extractResponse(response));
     }
 
+    /**
+     * @summary Send a get request to url wdata
+     * @param {string} url The url to send the request to
+     * @param {object} data The get data to send
+     * @returns {Promise<any>} The response
+     * @throws {Error} The status of the response is not "OK"
+     */
     function getRequest(url, data = {}) {
         return fetch(`api/${url}?${new URLSearchParams(data)}`)
             .then((response) => response.json())
-            .then((response) => {if (response.status !== "OK") {throw new Error(response.message)} return response.response;});
+            .then((response) => extractResponse(response));
     }
 
-    function login(username) {
-        return postRequest("login", {username});
+    /**
+     * @summary Throws an error if response.status !== "OK"
+     * @param {object} response The response to check
+     * @returns {any} response.response if no error occured
+     * @throws {Error} If response.status !== "OK"
+     */
+    function extractResponse(response) {
+        if (response.status !== "OK") {
+            throw new Error(response.message);
+        }
+
+        return response.response;
     }
 
-    function me() {
-        return getRequest("me");
-    }
+    // Update callbacks
+    //
+    /**
+     * @public
+     * @memberof module:comm
+     * @callback updateCallback
+     */
 
-    function mygroup() {
-        return getRequest("mygroup");
-    }
-
-    function rankedgroups() {
-        return getRequest("rankedgroups");
-    }
-
-    function leavegroup() {
-        return getRequest("leavegroup");
-    }
-
-    function invitegroup(group) {
-        return postRequest("invitegroup", {groupid: group.id});
-    }
-
-
+    /**
+     * @summary An array of registered callbacks
+     * @type {updateCallback}
+     */
     const callbacks = [];
+
+    /**
+     * @public
+     * @memberof module:comm
+     * @summary Register an update handler to be called every time the server has an update
+     * @param {updateCallback} callback The function to call on update
+     */
     function registerUpdateHandler(callback) {
         callbacks.push(callback);
     }
 
-    function executeCallbacks() {
-        callbacks.forEach((c) => c());
-    }
-
+    // When everything has loaded start the websocket and run the callbacks once
     window.addEventListener("load", () => {
         // Start websocket
         const socket = new WebSocket("ws://" + window.location.host + window.location.pathname);
@@ -55,7 +167,7 @@
             console.error("Could not create websocket!", e);
             console.log("Using a timer instead!");
             setInterval(executeCallbacks, 1000);
-        }
+        };
         socket.onopen = (e) => console.log("Opened websocket", e);
         socket.onclose = (e) => console.log("Closed websocket", e);
 
@@ -65,7 +177,13 @@
         executeCallbacks();
     });
 
+    /**
+     * @summary Executes all update-callbacks
+     */
+    function executeCallbacks() {
+        callbacks.forEach((c) => c());
+    }
 
-    if (window.commjs) {console.error("commjs loaded twice"); alert("commjs loaded twice"); return;}
+
     window.commjs = {login, me, mygroup, rankedgroups, leavegroup, invitegroup, registerUpdateHandler};
 }());
