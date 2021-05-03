8fcbb14be23ad147857b6488268c191ea214f0d4
Mati-AAU <matjen20@student.aau.dk>
2021-05-03 12:57:27 +0200

    Check for divide by 0 in preference algorithm

--- src/algorithms/preference.js
+++ src/algorithms/preference.js
@@ -29,5 +29,10 @@ function preferenceAlg(preferences){
     }
     const min = Math.min(fromHome, inOffice);
     const max = Math.max(fromHome, inOffice);
-    return -(min / max);
+    if (/*max === 0*/false){
+        return 0;
+    }
+    else {
+        return -(min / max);
+    }
 }
